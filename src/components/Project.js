0590ff74dc0b2c126f95d1b86ee00cca5eb1feb4
mikduc <80714781+mikduc@users.noreply.github.com>
2024-08-09 02:00:30 +0000

    centered a div :P

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ const Project = (props) => {
   return (
     <div className="project flex-container">
       <div className="icon" onClick={()=>{window.open(`${props.url}`)}} style={{cursor: "pointer"}}>
-        <img src={`${props.img}`} width="250px" height="250px" style={{zIndex: "2"}}/>
+        <img src={props.img} width="250px" height="250px" style={{zIndex: "2"}}/>
       </div>
       <h2 className="m text-secondary">{props.name}</h2>
       <h3 className="s text-secondary">{props.description}</h3>
